9d2337c7b4529fc3dac1680b6a27546f4cfd9057
Shadman <97025919+shadmanhussain05@users.noreply.github.com>
2026-07-25 17:29:36 +0530

    Backend

---
+++ ZiprunApplication.java
@@ -0,0 +1,13 @@
+package com.ziprun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiprunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiprunApplication.class, args);
+	}
+
+}
